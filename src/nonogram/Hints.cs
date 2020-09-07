932028bf9c0efa4752751d3756bfcc894d159897
Ben Lewis <me@lewisbj.com>
2020-09-07 20:37:38 +1000

    Restructured project

---
+++ src/nonogram/Hints.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nonogram
+{
+    [Serializable]
+    class Hint
+    {
+        private int[] blockLengths;
+
+        public int Length 
+        {
+            get 
+            {
+                return blockLengths.Length;
+            }
+        }
+
+        public int this[int i]
+        {
+            get
+            {
+                return blockLengths[i];
+            }
+        }
+
+        public Hint(int[] blockLengths)
+        {
+            this.blockLengths = blockLengths;
+        }
+
+        public int Occupation(int p = 0)
+        {
+            int sum = 0;
+            for (int i = p; i < blockLengths.Length; i++)
+            {
+                sum += blockLengths[i];
+            }
+            return sum + (blockLengths.Length - 1 - p);
+        }
+    }
+
+    [Serializable]
+    class HintSet
+    {
+        Hint[] hintArray;
+
+        public int Length => hintArray.Length; 
+
+        public Hint this[int i]
+        {
+            get
+            {
+                return hintArray[i];
+            }
+        }
+
+        public HintSet(Hint[] hintArray)
+        {
+            this.hintArray = hintArray;
+        }
+    }
+}
